62daeb50cd01ff28b9a8c152bd293ca83f127097
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2018-10-11 00:03:48 +0530

    Boiler plate code - Part II

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//App.js
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600})
+    mainWindow.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
+
+app.on('activate', function() {
+    if(mainWindow === null) {
+        createWindow()
+    }
+})
+
+app.on('closed', function() {
+    mainWindow = null
+})
--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-//Index.js
+var $ = require('jquery');
+var fs = require('fs');
+const {
+    dialog
+} = require('electron').remote;
+
+$(document).ready(() => {
+    console.log("***App Ready***");
+
+    $('#source-folder').on('click', (event) => {
+        var sourceDirectory = event.currentTarget.value;
+        console.log("***Source Directory***");
+        dialog.showOpenDialog({
+            title: "Select the Source Folder",
+            properties: ["openDirectory"]
+        }, (folderPaths) => {
+            if(folderPaths === undefined) {
+                console.log("***No Source Folder Selected***");
+                return;
+            } else {
+                console.log(folderPaths);
+                printFolderName(folderPaths, 'source-folder-value');
+            }
+        });
+
+    });
+    
+    $('#destination-folder').on('click', (event) => {
+        var sourceDirectory = event.currentTarget.value;
+        console.log("***Destination Directory***");
+        dialog.showOpenDialog({
+            title: "Select the Destination Folder",
+            properties: ["openDirectory"]
+        }, (folderPaths) => {
+            if(folderPaths === undefined) {
+                console.log("***No Destination Folder Selected***");
+                return;
+            } else {
+                console.log(folderPaths);
+                printFolderName(folderPaths, 'destination-folder-value');
+            }
+        });
+
+    });
+
+
+});
+
+function printFolderName(folderName, targetElement) {
+    targetElement && $('#'+targetElement).text(folderName);
+}
