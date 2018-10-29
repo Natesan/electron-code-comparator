cf2955743604cd4698f0cbc77bbf50150ee2e1f3
natesan.sivagnanam <natesan.sivagnanam@sap.com>
2018-10-29 22:04:53 +0530

    UI enhancements, bug fixes, handled presentation issues

--- compare.js
+++ compare.js
@@ -4,27 +4,24 @@ var fs = require('fs');
 var jsdiff = require('diff');
 var writeFile = require('write');
 
-//let rootDir = process.cwd();
-
-var aFileList = [];
-var aFinalList = [];
-var aAddedFileList = [];
-var aRemovedFileList = [];
-var aSourceFileList = [];
-var aTargetFileList = [];
-var aDiffContent = [];
-var sMode = '';
+var aFileList, aFinalList, aAddedFileList, aRemovedFileList, aSourceFileList, aTargetFileList, aDiffContent;
 var sourceSettings, targetSettings;
 var report;
 
-var constants = {
-    COMPARE_DIRECTORIES_ONLY: "Compare Directories Only",
-    COMPARE_DIRECTORIES_REPORT: "Compare Directories and Report",
-    COMPARE_DIRECTORIES_REPORT_DIFF: "Compare Directories, Report differences & Create Patch File"
-};
-
+var fnResetContent = function () {
+    aFileList = [];
+    aFinalList = [];
+    aAddedFileList = [];
+    aRemovedFileList = [];
+    aSourceFileList = [];
+    aTargetFileList = [];
+    aDiffContent = [];
+}
 
 var fnInitialize = function (srcDir, tarDir, fnCallBack) {
+
+    fnResetContent();
+
     sourceSettings = {
         root: srcDir,
         entryType: 'files',
@@ -36,8 +33,6 @@ var fnInitialize = function (srcDir, tarDir, fnCallBack) {
         entryType: 'files',
         filter: '*.js'
     };
-
-    //sMode = answers.action;
 
     fnAsyncDirectoryRead(sourceSettings).then(function (result) {
         aSourceFileList = result;
@@ -64,20 +59,20 @@ var fnCompareFileList = function (srcFileList, destFileList) {
             }
         }));
 
-    console.log("\nFinal Matching List:" + aFinalList.length);
+    console.log(`\nFinal Matching List:${aFinalList.length}`);
     console.log(aFinalList);
 
     aAddedFileList = _.difference(destFileList, srcFileList);
-    console.log("\nAdded in Destination:" + aAddedFileList.length);
+    console.log(`\nAdded in Destination:${aAddedFileList.length}`);
     console.log(aAddedFileList);
 
     aRemovedFileList = _.difference(srcFileList, destFileList);
-    console.log("\nDeleted from Source:" + aRemovedFileList.length);
+    console.log(`\nDeleted from Source:${aRemovedFileList.length}`);
     console.log(aRemovedFileList);
 }
 
 var fnCompareFileContent = function (srcDir, tarDir) {
-    console.log("\nComparing File Content for Files available in both directories...");
+    console.log(`\nComparing File Content for Files available in both directories...`);
 
     aFinalList.forEach((aFinalListItem) => {
         var srcFileContent = fs.readFileSync(srcDir + '/' + aFinalListItem, "utf8");
@@ -116,54 +111,35 @@ var fnWriteReport = function () {
     var sReportContent = [];
     var sDiffContent = [];
 
-    console.log("\nFinal Matching List:" + aFinalList.length);
-    console.log("\nAdded in Destination:" + aAddedFileList.length);
-    console.log("\nDeleted from Source:" + aRemovedFileList.length);
+    console.log(`\nFinal Matching List:${aFinalList.length}`);
+    console.log(`\nAdded in Destination:${aAddedFileList.length}`);
+    console.log(`\nDeleted from Source:${aRemovedFileList.length}`);
 
-    sReportContent.push("\nFinal Matching List:" + aFinalList.length);
-    sReportContent.push("\nAdded in Destination:" + aAddedFileList.length);
-    sReportContent.push("\nDeleted from Source:" + aRemovedFileList.length);
+    sReportContent.push(`\nFinal Matching List:${aFinalList.length}`);
+    sReportContent.push(`\nAdded in Destination:${aAddedFileList.length}`);
+    sReportContent.push(`\nDeleted from Source:${aRemovedFileList.length}`);
 
-    sReportContent.push("\n\nFinal Matching List:\n");
+    sReportContent.push(`\n\nFinal Matching List:\n`);
     aFinalList.forEach((finalListItem) => {
         sReportContent.push(finalListItem + '\n');
     });
 
-    sReportContent.push("\n\nAdded in Destination:\n");
+    sReportContent.push(`\n\nAdded in Destination:\n`);
     aAddedFileList.forEach((addedListItem) => {
         sReportContent.push(addedListItem + '\n');
     });
 
-    sReportContent.push("\n\nDeleted from Source:\n");
+    sReportContent.push(`\n\nDeleted from Source:\n`);
     aRemovedFileList.forEach((removedListItem) => {
         sReportContent.push(removedListItem + '\n');
     });
 
-    // if (sMode === constants.COMPARE_DIRECTORIES_REPORT || sMode === constants.COMPARE_DIRECTORIES_REPORT_DIFF) {
-    //     console.log("\nWriting Report to File...");
-    //     writeFile('result/result.txt', sReportContent, function (err) {
-    //         if (err) console.log(err);
-    //     });
-    // }
-
-
-    // if (sMode === constants.COMPARE_DIRECTORIES_REPORT_DIFF) {
-    //     aDiffContent.forEach((diffItem) => {
-    //         sDiffContent.push("\n\nDifference between '" + diffItem.oldHeader + "' and '" + diffItem.newHeader + "'\n");
-    //         sDiffContent.push(diffItem.hunks[0].lines);
-    //     });
-    //     console.log("\nCreating Patch File...");
-    //     writeFile('result/diff.patch', sDiffContent, function (err) {
-    //         if (err) console.log(err);
-    //     });
-    // }
-
     aDiffContent.forEach((diffItem) => {
-        sDiffContent.push("\n\nDifference between '" + diffItem.oldHeader + "' and '" + diffItem.newHeader + "'\n");
+        sDiffContent.push(`\n\nDifference between ${diffItem.oldHeader} and ${diffItem.newHeader} \n`);
         sDiffContent.push(diffItem.hunks[0].lines.join(""));
     });
 
-    console.log("\nCreating Patch File...");
+    console.log(`\nCreating Patch File...`);
     writeFile('download/diff.patch', sDiffContent.join(""), function (err) {
         if (err) console.log(err);
     });
@@ -171,12 +147,11 @@ var fnWriteReport = function () {
     return {
         reportContent: sReportContent,
         diffContent: sDiffContent,
-        filesMatched: aFinalList,
-        filesAddedDestination: aAddedFileList,
-        filesRemovedSource: aRemovedFileList
+        filesMatched: aFinalList.sort(),
+        filesAddedDestination: aAddedFileList.sort(),
+        filesRemovedSource: aRemovedFileList.sort()
 
     };
 }
 
 exports.initializeComparison = fnInitialize;
-//exports.compareDirectories = fnCompareDirectories;
